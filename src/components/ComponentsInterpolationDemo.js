0ec6ab8271004a886d2d518be9e27f3cea2da986
totigm <totigm@gmail.com>
2024-01-23 14:40:20 -0300

    Update demo

--- src/components/ComponentsInterpolationDemo.js
+++ src/components/ComponentsInterpolationDemo.js
@@ -3,21 +3,27 @@ import styled from '../styled';
 
 export default function ComponentsInterpolationDemo() {
   return (
-    <Wrapper>
+    <OuterWrapper>
+      <Wrapper>
+        <TextWrapper>
+          Components interpolation demo with wrapper
+          <Emoji>😅</Emoji>
+        </TextWrapper>
+      </Wrapper>
       <TextWrapper>
-        Components interpolation demo
+        Components interpolation demo without wrapper
         <Emoji>😅</Emoji>
       </TextWrapper>
-    </Wrapper>
+    </OuterWrapper>
   );
 }
 
-const Wrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
+const OuterWrapper = styled.div`
+  border: 1px solid red;
+  border-radius: 8px;
+`;
 
-  padding: 12px;
+const Wrapper = styled.div`
   background-color: hsl(180deg 80% 40%);
   border-radius: 8px;
 `;
@@ -25,13 +31,19 @@ const Wrapper = styled.div`
 const Emoji = styled.span`
   transition: transform 200ms;
   display: block;
+
+  ${Wrapper} & {
+    transform: rotate(180deg);
+  }
 `;
 
-const TextWrapper = styled.div`
+const TextWrapper = styled.span`
   display: flex;
   gap: 4px;
   align-items: center;
-  font-size: 1.1rem;
+  justify-content: center;
+  font-size: 1.3rem;
+  padding: 12px;
 
   &:hover {
     color: hsl(350deg 100% 40%);
